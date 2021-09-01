db4604b098e149f9eae295b494e4ac42be415fc9
mohammed-sas <89216938+mohammed-sas@users.noreply.github.com>
2021-09-01 11:26:07 +0530

    setting view and view engine

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const homeController = require('../controllers/home_controller');
-router.get('/',homeController.home);
 
+router.get('/',homeController.home);
+router.use('/users',require('./users'));
+// for any further routers
+// router.use('/routerName',require('./routerfile'))
+router.use('/post',require('./post'));
 console.log('route loaded');
 module.exports = router;
