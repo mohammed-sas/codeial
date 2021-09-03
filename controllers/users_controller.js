69149d1fe6b701cae22eb5c22a07ccad1700cc4d
mohammed-sas <89216938+mohammed-sas@users.noreply.github.com>
2021-09-03 13:55:19 +0530

    creating user router for sign in and sign up

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,51 @@
+const User = require('../models/user.js');
+
 module.exports.profile = function (req,res){
-    res.end("<h1>User profile</h1>");
+    res.render('users',{
+        title: "User Homepage"
+    });
+};
+
+module.exports.signUp = function(req,res){
+    res.render('user_sign_up',{
+        title:"Codeial | Sign up"
+    });
+};
+
+module.exports.signIn = function(req,res){
+    res.render('user_sign_in',{
+        title :"Codeial | Sign in"
+    });
+};
+
+// get signup data
+module.exports.create = function(req,res){
+    if(req.body.password != req.body.confirm_password){
+      return  res.redirect('back');
+    }
+
+    User.findOne({email :req.body.email},function(err,user){
+        if(err){
+            console.log(`error in finding user in signing up ${err}`);
+            return;
+        }
+
+        if(!user){
+            User.create(req.body,function (err,user){
+                if(err){
+                    console.log(`error in finding user in signing up ${err}`);
+                    return;
+                }
+                
+                return res.redirect('/users/sign-in');
+            });
+        }else{
+            return res.redirect('back');
+        }
+    });
+
+};
+
+module.exports.createSession = function(req,res){
+
 };
