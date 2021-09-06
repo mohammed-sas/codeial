ef4099ec6854602e9f6a3ce646738020a27bccf7
mohammed-sas <89216938+mohammed-sas@users.noreply.github.com>
2021-09-06 14:44:24 +0530

    post controller update

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,3 +1,18 @@
-module.exports.post = function(req,res){
-    res.send("<h1> Post section</h1>");
+
+const Post = require('../models/post');
+
+module.exports.create = function(req,res){
+    Post.create(
+        {
+            content : req.body.content,
+            user: req.user._id
+        }, function(err ,post){
+            if(err){
+                console.log(`error in creating post`);
+                return;
+            }
+
+            return res.redirect('back');
+        }
+    );
 };
