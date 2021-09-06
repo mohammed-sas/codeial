77e78a2b9a928df1efbb9c516cd2ad06e1666d1b
mohammed-sas <89216938+mohammed-sas@users.noreply.github.com>
2021-09-06 14:44:53 +0530

    adding router for post

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const homeController = require('../controllers/home_controller');
 
 router.get('/',homeController.home);
 router.use('/users',require('./users'));
+
 // for any further routers
 // router.use('/routerName',require('./routerfile'))
 router.use('/post',require('./post'));
--- routes/post.js
+++ routes/post.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const postController = require('../controllers/post_controller');
 
-router.get('/',postController.post);
+router.post('/create',postController.create);
 
 module.exports = router;
