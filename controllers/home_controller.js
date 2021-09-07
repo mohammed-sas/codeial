55aeb0e0b071ab0624457e38e395d626da53f058
mohammed-sas <89216938+mohammed-sas@users.noreply.github.com>
2021-09-07 19:42:26 +0530

    fix : bug

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,21 +1,38 @@
-
 const Post = require('../models/post');
-module.exports.home = function (req,res){
+const User = require('../models/user');
+module.exports.home = async function (req, res) {
 
-    // Post.find({}, function (err,posts){
+    // Post.find({}, function (err, posts) {
+    //     if (err) {
+    //         console.log(`error ${err}`);
+    //         return;
+    //     }
     //     console.log(posts);
-    //     return res.render('home',{
-    //         title : "Codeial | Home",
-    //         posts : posts
-    // });
-    // });
-   console.log(Post.find({}));
-    Post.find({}).populate('user').exec(function (err,posts){
-               
 
-        return res.render('home',{
-                    title : "Codeial | Home",
-                    posts : posts
-         });
-    })
+    //     return res.render('home', {
+    //         title: "Codeial | Home",
+    //         posts: posts
+    //     });
+    // });
+    //    console.log(Post.find({}));
+    Post.find({}).populate('user').exec(function (err, posts) {
+
+        if(err){
+            console.log(`error in side populate ${err}`);
+            return;
+        }
+        return res.render('home', {
+            title: "Codeial | Home",
+            posts: posts
+        });
+    });
+
+    // let posts = await Post.find({}).populate('user');
+
+
+    
+    // return res.render('home',{
+    //                     title : "Codeial | Home",
+    //                     posts : posts
+    //          });
 }
